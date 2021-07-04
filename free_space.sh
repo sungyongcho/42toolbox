3a0c209d022fb3acd91d44194b18f2cecf2d8b81
Sungyong Cho <to.sungyongcho@gmail.com>
2021-07-04 16:30:30 +0900

    smun added

--- free_space.sh
+++ free_space.sh
@@ -44,6 +44,13 @@ delete "./Library/Containers/*"
 delete "./Downloads/*"
 delete "./.Trash/*"
 
+# Delete more from: @smun
+rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage
+rm -rf ~/Library/Application\ Support/discord/Cache
+rm -rf ~/Library/Application\ Support/Code/CachedData
+rm -rf ~/Library/Developer/CoreSimulator
+
 # Brew cleanup
 read -n1 -p "${blue}Cleanup Homebrew? (${cyan}brew cleanup${blue}) [y/${cyan}N${blue}]${reset} " input
 echo ""
